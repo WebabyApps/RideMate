8897eb0a5b155c9ad16db8c167912a725a680248
Webaby <webabyrocks@gmail.com>
2025-10-13 13:27:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1835169929-c35ab",
+  "appId": "1:648798500393:web:88d20a7c714aaf185d8e38",
+  "apiKey": "AIzaSyCQ_p4o-gwx__Eicy12UZIGouYfy74ba5k",
+  "authDomain": "studio-1835169929-c35ab.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "648798500393"
+};
