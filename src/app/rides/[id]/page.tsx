8e9bdba2f80986ac808e6cf847159e5e6a22b93c
Webaby <webabyrocks@gmail.com>
2025-10-13 20:54:38 +0000

    FirebaseError: Missing or insufficient permissions: The following reques

--- src/app/rides/[id]/page.tsx
+++ src/app/rides/[id]/page.tsx
@@ -97,7 +97,7 @@ export default function RideDetailPage() {
       }, 0);
       return () => clearTimeout(timer);
     }
-  }, [isRideLoading, ride, notFound]);
+  }, [isRideLoading, ride]);
 
 
   const handleBookSeat = () => {
